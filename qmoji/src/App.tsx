c5ab689ba29a26b42ea719ca62059b98da09769f
kuokuoli <kuokuoli@stanford.edu>
2026-08-03 16:36:19 +0200

    some extra layering

--- qmoji/src/App.tsx
+++ qmoji/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+{/*import { useState, useEffect } from 'react';
 import './App.css';
 import Start from './Start';
 
@@ -251,5 +251,22 @@ function App() {
     </main>
   );
 }
+
+import { Routes, Route } from 'react-router-dom';
+import SingleplayerGame from './SingleplayerGame';
+import MultiplayerLobby from './MultiplayerLobby';
+import Room from './Room';
+import './App.css';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<SingleplayerGame />} />
+      <Route path="/multiplayer" element={<MultiplayerLobby />} />
+      <Route path="/room/:roomId" element={<Room />} />
+    </Routes>
+  );
+}
+
 
 export default App;
